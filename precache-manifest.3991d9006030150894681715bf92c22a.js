837c5cc2f8b0794a2966245088458b05fd6b1f5b
nick momotenko <nickmomotenkoforweb@gmail.com>
2020-06-30 13:31:22 +0300

    Updates

---
+++ precache-manifest.3991d9006030150894681715bf92c22a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9921c5330971c40f352045246e8e06b8",
+    "url": "/react-festival-app/index.html"
+  },
+  {
+    "revision": "5082b7e10dc44b2ba9f8",
+    "url": "/react-festival-app/static/css/main.ed293ab2.chunk.css"
+  },
+  {
+    "revision": "0f7b256c5f8ae6aec028",
+    "url": "/react-festival-app/static/js/2.ed90178e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-festival-app/static/js/2.ed90178e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5082b7e10dc44b2ba9f8",
+    "url": "/react-festival-app/static/js/main.ec7af37e.chunk.js"
+  },
+  {
+    "revision": "18c2d38509f81b7596f9",
+    "url": "/react-festival-app/static/js/runtime-main.03518f22.js"
+  },
+  {
+    "revision": "47fc780386fb78a1431d87de0ea34cbc",
+    "url": "/react-festival-app/static/media/2.47fc7803.png"
+  },
+  {
+    "revision": "ab3eb181e614eb24560f59b7ecbfc171",
+    "url": "/react-festival-app/static/media/2.ab3eb181.jpg"
+  },
+  {
+    "revision": "79ff044665db23108687258b27b339b2",
+    "url": "/react-festival-app/static/media/3.79ff0446.jpg"
+  },
+  {
+    "revision": "662a4f1472618eb2eb3b507d7459741f",
+    "url": "/react-festival-app/static/media/instagram.662a4f14.svg"
+  },
+  {
+    "revision": "6c15e1259464e5f097bc8f32f848294d",
+    "url": "/react-festival-app/static/media/tw-1.6c15e125.svg"
+  },
+  {
+    "revision": "be06689853aec102607f5e54e6fd2dfa",
+    "url": "/react-festival-app/static/media/twitter.be066898.svg"
+  },
+  {
+    "revision": "9d516b2a798c01f50877ae939bee3102",
+    "url": "/react-festival-app/static/media/vimeo.9d516b2a.svg"
+  },
+  {
+    "revision": "171c8efea95ecc97796a5d63ac98af8d",
+    "url": "/react-festival-app/static/media/vk.171c8efe.svg"
+  }
+]);
